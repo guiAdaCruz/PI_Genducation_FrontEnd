ba126e06797981997c64e04e7d3ced9a5f48fda9
Kathleen-Fidelis <lucas.melo195@gmail.com>
2022-01-20 15:17:37 -0300

    @LucasCostaMelo [Task: Criando Rotas]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '', redirectTo: 'entrar', pathMatch: 'full'},
+{path: 'entrar', component: EntrarComponent},
+{path: 'cadastrar', component: CadastrarComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
