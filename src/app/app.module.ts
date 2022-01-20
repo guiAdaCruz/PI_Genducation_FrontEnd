f5e63ab52580b03608847932e3f51e5e589d9c31
unknown <fabriciorochapalmeira@gmail.com>
2022-01-20 14:18:32 -0300

    @fabricior0cha [Task: Criando componente rodapé]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EntrarComponent
+    EntrarComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
