915f9e97bdcfdc0ee4be9b1613ae2c547ad01c20
Kathleen-Fidelis <lucas.melo195@gmail.com>
2022-01-20 15:27:52 -0300

    @LucasCostaMelo [Task: Implementação de rotas]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { ContatoComponent } from './contato/contato.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
 const routes: Routes = [
 {path: '', redirectTo: 'entrar', pathMatch: 'full'},
 {path: 'entrar', component: EntrarComponent},
-{path: 'cadastrar', component: CadastrarComponent}
-
+{path: 'cadastrar', component: CadastrarComponent},
+{path: 'contato', component: ContatoComponent}
 
 ];
 
