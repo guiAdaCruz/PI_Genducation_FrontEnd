170255317714e654767d6264283d21019a7863d2
unknown <gui.albuquerque.gui@gmail.com>
2022-02-02 09:06:06 -0300

    @guiAdaCruz [Task: Corrigindo conflitos de merge]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,17 +12,9 @@ import { FormsModule } from "@angular/forms";
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { InicioComponent } from './inicio/inicio.component';
 import { TemaComponent } from './tema/tema.component';
-<<<<<<< HEAD
 import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
-=======
-
 import { PostagemComponent } from './postagem/postagem.component';
-import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
-import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
-import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
-import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
->>>>>>> 11fb20983030b92ba6213746be660ed91018938d
 
 @NgModule({
   declarations: [
@@ -34,6 +26,7 @@ import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component'
     RodapeComponent,
     InicioComponent,
     TemaComponent,
+    PostagemComponent,
     PostagemEditComponent,
     PostagemDeleteComponent
   ],
